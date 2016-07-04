24072e23edcb56bb610f6ef85bb34b96ed8950ee
Brian Tucker <brian@tucker.name>
2016-07-04 16:32:18 -0500

    Fixed PSR-2 formatting

--- tests/ClassNameTest.php
+++ tests/ClassNameTest.php
@@ -1,13 +1,10 @@
 <?php
-
 use CodeKitchen\PackageName\ClassName;
 
 class ClassNameTest extends PHPUnit_Framework_TestCase {
- 
   public function testClassNameHasResult()
   {
     $class_name = new ClassName;
     $this->assertTrue($class_name->functionName());
   }
- 
 }
